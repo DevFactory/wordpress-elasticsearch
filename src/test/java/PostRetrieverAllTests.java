a2acea2df0f1fb5932c9ff3eae991d368a9cfd52
Sebastian Faltoni <sebastian.faltoni@gmail.com>
2015-02-04 01:22:51 +0100

    renamed test

--- src/test/java/PostRetrieverAllTests.java
+++ src/test/java/PostRetrieverAllTests.java
@@ -45,7 +45,7 @@ public class PostRetrieverAllTests {
     }
 
     @Test
-    public void ResultSet() throws SQLException, ParseException {
+    public void ShouldReturnPostVerifyExecuteAndResultSet() throws SQLException, ParseException {
         final Date startDate = getDate();
         Post expectedPost = getPost(startDate);
         Connection connection = getConnection();
